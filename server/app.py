a162095d265367df2a4d72fb4a96339331fb2250
Nick Weiland <njweiland23@gmail.com>
2024-01-22 15:33:16 -0600

    added /swipes route

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, Users, Recipes, Swipes
 
 
 # Views go here!
@@ -16,6 +17,23 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+@app.route('/swipes', methods = ['POST'])
+def swipes():
+    form_data = request.get_json()
+    new_swipes = Swipes(
+        swipe = form_data['swipe'],
+        user_id = form_data['user_id'],
+        recipe_id = form_data['recipe_id'],
+        swipe_date = form_data['swipe_date']
+    )
+    db.session.add(new_swipes)
+    db.session.commit()
+    response = make_response(
+        new_swipes.to_dict(),
+        201
+    )
+    return response
 
 
 if __name__ == '__main__':
