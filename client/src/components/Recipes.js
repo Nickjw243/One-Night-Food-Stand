cee4874c71eebe5b0b4f8d8c82a08004b5416fed
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-23 15:28:55 -0500

    recipes.js passing props

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -11,6 +11,12 @@ function Recipes() {
         .then((data) => setRecipes(data))
         .catch((error) => console.error('Error fetching recipes:', error))
     }, [])
+
+    return (
+        <div>
+            <SwipePage recipes = {recipes} />
+        </div>
+    )
 }
 
 export default Recipes;
