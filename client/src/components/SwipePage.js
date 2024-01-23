f4151b3edb6768b06b000f5c9cc0f8b1374c41c3
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-23 15:22:41 -0500

    swipepage.js functions

--- client/src/components/SwipePage.js
+++ client/src/components/SwipePage.js
@@ -2,14 +2,28 @@ import React, { useEffect, useState } from "react";
 import Matches from "./Matches"
 import Filters from "./Filters"
 
-function SwipePage({ currentUser, onAddSwipe}) {
-    const [swipes, setSwipes] = useState("");
+function SwipePage({ }) {
+    const [recipes, setRecipes] = useState([])
+    const [currentRecipeIndex, setCurrentRecipeIndex] = useState(0)
 
     useEffect(() => {
-        fetch('/swipes')
-        .then(r => r.json())
-        .then(data => setSwipes(data))
+        //Fetch recipes from the server
+        fetch('/recipes')
+        .then((res) => res.json())
+        .then((data) => setRecipes(data))
+        .catch((error) => console.error('Error fetching recipes:', error))
     }, [])
+
+    function handleSwipeLeft() {
+        setCurrentRecipeIndex((prevIndex) => (prevIndex + 1) % recipes.length)
+    }
+
+    function handleSwipeRight() {
+        setCurrentRecipeIndex((prevIndex) => (prevIndex + 1) % recipes.length)
+    }
+
+
+
     // function handleSubmit(e) {
     //     e.preventDefault()
 
