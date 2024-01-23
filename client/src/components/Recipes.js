365427276620b24b6e820abc2fdc449b3ff5d38d
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-23 15:27:31 -0500

    recipes.js functions

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import SwipePage from "./SwipePage";
 
-function Recipes() {}
+function Recipes() {
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        //Fetch recipes from the server
+        fetch('/recipes')
+        .then((res) => res.json())
+        .then((data) => setRecipes(data))
+        .catch((error) => console.error('Error fetching recipes:', error))
+    }, [])
+}
 
 export default Recipes;
